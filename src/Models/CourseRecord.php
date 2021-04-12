b1d3d705f3a1e155bee25eed1db4967d2f89f179
heshuo <1046579232@qq.com>
2021-04-12 13:24:56 +0800

    获取录制状态添加异常情况

---
+++ src/Models/CourseRecord.php
@@ -0,0 +1,12 @@
+<?php namespace Vipbressanon\LiveTool\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class CourseRecord extends Model
+{
+    public $timestamps = true;
+
+    protected $table = 'course_record';
+
+    protected $guarded = [];
+}
